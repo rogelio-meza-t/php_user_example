7dde6b8e916b9bd3d29822e23ba9c73219b04466
Rogelio Meza <rogelio.meza.t@gmail.com>
2017-07-09 21:49:54 -0400

    show not found page

--- index.php
+++ index.php
@@ -59,6 +59,8 @@ $uri = rawurldecode($uri);
 $routeInfo = $dispatcher->dispatch($httpMethod, $uri);
 switch ($routeInfo[0]) {
     case FastRoute\Dispatcher::NOT_FOUND:
+        header('HTTP/1.0 404 Not Found');
+        render('errors', '404');
         break;
     case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
         $allowedMethods = $routeInfo[1];
